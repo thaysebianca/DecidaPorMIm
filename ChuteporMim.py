29049f937c545e9e4a2c79ddd70e77efb3e0c8ef
thayse <Thayse Bianca>
2021-01-11 16:59:47 -0300

    DecidaPorMim

---
+++ ChuteporMim.py
@@ -0,0 +1,17 @@
+from random import randint
+
+numero = randint(1,11)
+
+print("Olá, este é um jogo de adivinhação de 0 a 10.")
+
+correto = False
+chutes = 0
+
+while not correto:
+    deducao = int(input("Digite um número: "))
+    chutes += 1
+    if deducao == numero:
+        correto = True
+
+print("Isso mesmo! Você é bom(a) de adivinhação!")
+print("Número de palpites: {0}".format(chutes))
